df1b6290476c197fc97f86c5308f80e0d3432d93
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-04-03 06:26:27 +0530

    First class we discussed loops

---
+++ Coding/Kapish11/InfiniteForLoop.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class InfiniteForLoop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class InfiniteForLoop
+{
+    public static void main(String[] args){
+        System.out.print("\f");
+        for(;;){
+            System.out.print("\nHello!, I am Infinite loop");
+        }
+    }
+}
