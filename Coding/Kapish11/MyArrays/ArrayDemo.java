7d6048862c8b3041d3523c7695efab09ae318227
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-07-17 06:54:43 +0530

    Discussed Array concept in Java, with homework consist of 8 questions

---
+++ Coding/Kapish11/MyArrays/ArrayDemo.java
@@ -0,0 +1,22 @@
+package MyArrays;
+
+
+/**
+ * Write a description of class ArrayDemo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ArrayDemo
+{
+    public static void main(String args[]){
+        int[] A, B, C; //Multiple Array Declaration
+        int X[], Y, Z; //Only X is Declared as an Array
+        //B = 5;
+        Y = 5;
+        A = new int[5];
+        B = new int[3];
+        int M[] = new int[10];
+        int[] N = new int[20];
+    }
+}
