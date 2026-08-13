92445fcf8c39d0560a7b8a3195cbc6764352f9e9
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-08-13 07:03:56 +0530

    Strings concept in Java

---
+++ Coding/Kapish11/MyStrings/MyStringOne.java
@@ -0,0 +1,23 @@
+package MyStrings;
+import java.util.*;
+
+
+/**
+ * Write a description of class MyStringOne here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyStringOne
+{
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        String s1 = "Welcome";
+        String s2 = "Welcome"; //instance doesn't create
+        System.out.print("\n"+s1+" " + s2);
+        
+        char ch[] = {'C', 'a', 't'};
+        
+        String pet = 
+    }
+}
---
+++ Coding/Kapish11/MyStrings/Template.java
@@ -0,0 +1,17 @@
+package MyStrings;
+import java.util.*;
+
+
+/**
+ * Write a description of class Template here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Template
+{
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.print("\f");
+    }
+}
